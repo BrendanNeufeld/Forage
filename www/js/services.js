7e0fbfc1cb496cb3ff9a6e38eeb25b67a7cef6f1
Christopher Lewis <chris@gomangostudios.net>
2014-07-10 00:02:44 -0400

    Added recipes and hints to service

--- www/js/services.js
+++ www/js/services.js
@@ -5,7 +5,6 @@ angular.module('starter.services', [])
  */
     .factory('Friends', function () {
         // Might use a resource here that returns a JSON array
-
         // Some fake testing data
         var friends = [
             { id: 0, name: 'Scruff McGruff' },
@@ -30,14 +29,21 @@ angular.module('starter.services', [])
 
         // Some fake testing data
         var ingredients = [
-            { id: "morel", uuid:'', name: "Morels", image: "img/Morchella.jpg", description: "Morchella, the true morels, is a genus of edible mushrooms closely related to anatomically simpler cup fungi. These distinctive mushrooms appear honeycomb-like in that the upper portion is composed of a network of ridges with pits between them. The ascocarps are prized by gourmet cooks, particularly for French cuisine. Commercial value aside, morels are hunted by thousands of people every year simply for their taste and the joy of the hunt."},
-            { id: "asparagus", uuid:'', name: "Wild Asparagus", image: "img/asparagus-spear.jpg", description: "Asparagus officinalis is a spring vegetable, a flowering perennial[1] plant species in the genus Asparagus. It was once classified in the lily family, like its Allium cousins, onions and garlic, but the Liliaceae have been split and the onion-like plants are now in the family Amaryllidaceae and asparagus in the Asparagaceae. Asparagus officinalis is native to most of Europe, northern Africa and western Asia, and is widely cultivated as a vegetable crop."},
-            { id: "garlic", uuid:'', name: "Wild Garlic", image: "img/Allium_canadense.jpg", description: "Wild onion (Allium canadense), also known as Canada onion, wild garlic, meadow garlic, and Canadian garlic,[3] is a perennial plant native to eastern North America from Texas to Florida to New Brunswick to Montana. The species is also cultivated in other regions as an ornamental and as a garden culinary herb.[4] The plant is also reportedly naturalized in Cuba."},
-            { id: "redfife",  uuid:'', name: "Red Fife Wheat", image: "img/red-fife-1.jpg", description: "Red Fife is a cultivar of bread wheat that originated in Peterborough, Ontario in 1842. It is believed to have crossed several continents and the Atlantic before arriving in Canada, where it gained a foothold on the land of David Fife, from which it is named."},
-            { id: "fiddleheads", uuid:'', name: "Fiddlehead Greens", image: "img/fiddlehead.jpg", description: "Fiddlehead greens are the furled fronds of a young fern,[1] harvested for use as a vegetable. Left on the plant, each fiddlehead would unroll into a new frond (circinate vernation)"}
+            { id: "morel", isCollected:false, hints:"<ul><li>hint</li></ul>", uuid:'', name: "Morels", image: "img/Morchella.jpg", description: "Morchella, the true morels, is a genus of edible mushrooms closely related to anatomically simpler cup fungi. These distinctive mushrooms appear honeycomb-like in that the upper portion is composed of a network of ridges with pits between them. The ascocarps are prized by gourmet cooks, particularly for French cuisine. Commercial value aside, morels are hunted by thousands of people every year simply for their taste and the joy of the hunt.", recipe:"This is my recipe. <br>isn't it fine!<ol><li>Instruction</li></ol>"},
+            { id: "asparagus", isCollected:false, hints:"<ul><li>hint</li></ul>", uuid:'', name: "Wild Asparagus", image: "img/asparagus-spear.jpg", description: "Asparagus officinalis is a spring vegetable, a flowering perennial[1] plant species in the genus Asparagus. It was once classified in the lily family, like its Allium cousins, onions and garlic, but the Liliaceae have been split and the onion-like plants are now in the family Amaryllidaceae and asparagus in the Asparagaceae. Asparagus officinalis is native to most of Europe, northern Africa and western Asia, and is widely cultivated as a vegetable crop.", recipe:"This is my recipe. <br>isn't it fine!<ol><li>Instruction</li></ol>"},
+            { id: "garlic", isCollected:false, hints:"<ul><li>hint</li></ul>", uuid:'', name: "Wild Garlic", image: "img/Allium_canadense.jpg", description: "Wild onion (Allium canadense), also known as Canada onion, wild garlic, meadow garlic, and Canadian garlic,[3] is a perennial plant native to eastern North America from Texas to Florida to New Brunswick to Montana. The species is also cultivated in other regions as an ornamental and as a garden culinary herb.[4] The plant is also reportedly naturalized in Cuba.", recipe:"This is my recipe. <br>isn't it fine!<ol><li>Instruction</li></ol>"},
+            { id: "redfife", isCollected:false, hints:"<ul><li>hint</li></ul>", uuid:'', name: "Red Fife Wheat", image: "img/red-fife-1.jpg", description: "Red Fife is a cultivar of bread wheat that originated in Peterborough, Ontario in 1842. It is believed to have crossed several continents and the Atlantic before arriving in Canada, where it gained a foothold on the land of David Fife, from which it is named.", recipe:"This is my recipe. <br>isn't it fine!<ol><li>Instruction</li></ol>"},
+            { id: "fiddleheads", isCollected:true, hints:"<ul><li>hint</li></ul>", uuid:'', name: "Fiddlehead Greens", image: "img/fiddlehead.jpg", description: "Fiddlehead greens are the furled fronds of a young fern,[1] harvested for use as a vegetable. Left on the plant, each fiddlehead would unroll into a new frond (circinate vernation)", recipe:"This is my recipe. <br>isn't it fine!<ol><li>Instruction</li></ol>"}
         ];
 
         return {
+			setCollected:function($id, $value){
+				for (var i=0; i<ingredients.length; i++){
+                    if(ingredientId == $id){
+                        ingredients[ingredientId].isCollected = $value;
+                    }
+                }
+			},
             allIngredients: function () {
                 return ingredients;
             },
@@ -49,8 +55,6 @@ angular.module('starter.services', [])
                 for (var i=0; i<ingredients.length; i++){
                     if(ingredientId == ingredients[i].id){
                         ingId = i;
-
-
                     }
                 }
                 if(ingId)   return ingredients[ingId]
